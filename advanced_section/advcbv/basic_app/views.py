26ccc60d98d00ba090374970ee4c4529c22a5ef0
mickiy123 <mikikinfe345@gmail.com>
2021-06-24 18:56:39 +0300

    in views.py created a class to see schoollist and details

--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import View,TemplateView,ListView,DetailView
+from . import models
 
 
 # Create your views here.
 
 
 
+class SchoolListView(ListView):
+    model = models.School
 
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'basic_app/school_detail.html'    
