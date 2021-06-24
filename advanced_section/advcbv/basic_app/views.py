b2e297d946a67f31e274dc53d074a2cb04ea368a
mickiy123 <mikikinfe345@gmail.com>
2021-06-24 16:46:13 +0300

    in the views.py i removed the function i created and imported view then wrote a new class with htmlresponse

--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
+
 
 # Create your views here.
 
-def index(request):
-    return render(request,"index.html")
+class CBview(View):
+    def get(self,request):
+        return HttpResponse("CLASS BASE VIEWS ARE COOL!")
+        
