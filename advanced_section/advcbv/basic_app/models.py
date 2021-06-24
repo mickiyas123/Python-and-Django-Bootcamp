8dfe5cfe2be9bb1cef738583517f356ad4524314
mickiy123 <mikikinfe345@gmail.com>
2021-06-24 18:05:56 +0300

    in models.py of our apps i cretaed two models assuming it's for school

--- advanced_section/advcbv/basic_app/models.py
+++ advanced_section/advcbv/basic_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=256)
+    principal = models.CharField(max_length=256)
+    location = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name 
+class Student(models.Model):
+    name = models.CharField(max_length=256)
+    age = models.PositiveIntegerField()
+    School = models.ForeignKey(School, related_name="Students")
+
+    def __str__(self):
+        return self.name
+        
+                
