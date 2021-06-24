c12452e6ca6948f24b075f34c8cca83e674aacc1
mickiy123 <mikikinfe345@gmail.com>
2021-06-24 18:50:41 +0300

    imported List and Detail View in views.py

--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView
+from django.views.generic import View,TemplateView,ListView,DetailView
 
 
 # Create your views here.
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
-    def get_context_data(self,**kwargs):
-        context = super().get_context_data(**kwargs)
-        context['injectme'] = 'BASIC INJECTION!'
-        return context
-        
+
 
 
