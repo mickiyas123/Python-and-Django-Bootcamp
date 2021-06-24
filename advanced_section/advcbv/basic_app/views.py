405e868d9f3d8b6c724d0cf2b34a1115b0ade759
mickiy123 <mikikinfe345@gmail.com>
2021-06-24 17:17:04 +0300

    in views.py imported and created a new class in urls.py wrote the class name i created in views.py

--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import View
-from django.http import HttpResponse
+from django.views.generic import View,TemplateView
 
 
 # Create your views here.
 
-class CBview(View):
-    def get(self,request):
-        return HttpResponse("CLASS BASE VIEWS ARE COOL!")
-        
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
