d141321b57d46a41825b1d082b66e00df2156654
mickiy123 <mikikinfe345@gmail.com>
2021-06-25 20:58:30 +0300

    in views.py i created a method to inject a content in the index.html file

--- advanced_section1/advcbv/basic_app/views.py
+++ advanced_section1/advcbv/basic_app/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context ['injectme'] = "Basic Injection!"
+        return context
