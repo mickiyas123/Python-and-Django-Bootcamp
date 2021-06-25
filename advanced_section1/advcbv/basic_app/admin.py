a52b0788ba60b93044baec8bdf19807143df6644
mickiy123 <mikikinfe345@gmail.com>
2021-06-25 21:20:22 +0300

    in admin.py of thr basicapp i imported the School and Student Models

--- advanced_section1/advcbv/basic_app/admin.py
+++ advanced_section1/advcbv/basic_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
+
+from .models import School,Student
 
 # Register your models here.
+
+admin.site,register(School)
+admin.site.register(Student)
