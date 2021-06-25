30a4d5b0d462a167a29a98679cd07b5af647c886
mickiy123 <mikikinfe345@gmail.com>
2021-06-25 16:01:38 +0300

    nothing new

--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -9,9 +9,11 @@ class IndexView(TemplateView):
     template_name = 'index.html'
 
 class SchoolListView(ListView):
+    context_object_name = 'schools'
     model = models.School
     # School List
 
 class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'    
