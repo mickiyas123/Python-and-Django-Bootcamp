34050a0b95252eee8eec2de1315a181ec116f953
mickiy123 <mikikinfe345@gmail.com>
2021-06-25 20:41:51 +0300

    After Removing the class based view i created a template based view so that i can access the html files in the templates folder

--- advanced_section1/advcbv/basic_app/views.py
+++ advanced_section1/advcbv/basic_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View,TemplateView
 from django.http import HttpResponse
 
 # Create your views here.
 
-
+class IndexView(TemplateView):
+    template_name = 'index.html'
