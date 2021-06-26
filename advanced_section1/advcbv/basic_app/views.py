0324246990c9d0be4dd0f289e4810bd9698ac618
mickiy123 <mikikinfe345@gmail.com>
2021-06-26 19:40:27 +0300

    Cretaed school create class to add new schools

--- advanced_section1/advcbv/basic_app/views.py
+++ advanced_section1/advcbv/basic_app/views.py
@@ -19,6 +19,9 @@ class SchoolListView(ListView):
 class SchoolDetailView(DetailView):
     context_object_name = "school_detail"
     model = models.School
-    template_name = 'basic_app/school_detail.html'        
+    template_name = 'basic_app/school_detail.html'
+
+class SchoolCreateView(CreateView):
+    model = models.School            
 
 
