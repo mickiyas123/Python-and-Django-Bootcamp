4741da4cf88e1e4b755017b919d4a8b803d0f2ef
mickiy123 <mikikinfe345@gmail.com>
2021-06-26 14:56:44 +0300

    Created a class for School list and deatil and renamed school detail html because i renamed it wrong

--- advanced_section1/advcbv/basic_app/views.py
+++ advanced_section1/advcbv/basic_app/views.py
@@ -8,4 +8,10 @@ from . import models
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+class SchoolListView(ListView):
+    model = models.School
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'basic_app/school_detail.html'        
+
 
