ee922903a160f5b692b42b5f1f8199f6403d5c76
mickiy123 <mikikinfe345@gmail.com>
2021-06-26 14:39:10 +0300

    in views.py file i removed the function beacuse we don't need to inject anything in our index.html file

--- advanced_section1/advcbv/basic_app/views.py
+++ advanced_section1/advcbv/basic_app/views.py
@@ -6,7 +6,4 @@ from django.http import HttpResponse
 
 class IndexView(TemplateView):
     template_name = 'index.html'
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context ['injectme'] = "Basic Injection!"
-        return context
+    
