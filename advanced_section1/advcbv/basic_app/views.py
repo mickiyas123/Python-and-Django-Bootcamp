4b9245991e1617fbab6e9673571c87aa570592d0
mickiy123 <mikikinfe345@gmail.com>
2021-06-26 14:39:51 +0300

    imported list and detail view

--- advanced_section1/advcbv/basic_app/views.py
+++ advanced_section1/advcbv/basic_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView
+from django.views.generic import View,TemplateView,ListView,DetailView
 from django.http import HttpResponse
 
 # Create your views here.
 
 class IndexView(TemplateView):
     template_name = 'index.html'
-    
+
