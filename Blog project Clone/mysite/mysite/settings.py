7c262866a5e4d7f9acdf9f4c4f1d9e79e0c72f60
mickiy123 <mikikinfe345@gmail.com>
2021-07-07 14:48:17 +0300

    in settings.py file of the project wrote a code to the static and template path

--- Blog project Clone/mysite/mysite/settings.py
+++ Blog project Clone/mysite/mysite/settings.py
@@ -10,10 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from os import path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = path.join(BASE_DIR,'blog/templates/blog')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = path.join(BASE_DIR,'static')
+
+
+LOGIN_REDIRECT_URL = '/'
