dcf2ceb49bfa5585e096d102fbda17bb0d3cacfc
mickiy123 <mikikinfe345@gmail.com>
2021-07-07 15:19:06 +0300

    imported the templateview and a class for accessing the templates

--- Blog project Clone/mysite/blog/views.py
+++ Blog project Clone/mysite/blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from blog.models import Post,Comment
+from django.views.generic import (TemplateView)
 
 # Create your views here.
+
+# a templateview for accessing the templates
+class Aboutview(TemplateView):
+    template_name = 'about.html'
+
 
 
