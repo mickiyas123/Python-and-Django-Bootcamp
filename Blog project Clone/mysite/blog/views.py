1b73fe96d07ce67974f96986d513063ba7f351d3
mickiy123 <mikikinfe345@gmail.com>
2021-07-07 16:10:32 +0300

    created a class for to list the post which is the home page and added a method for arranging post to the latest

--- Blog project Clone/mysite/blog/views.py
+++ Blog project Clone/mysite/blog/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.views.generic.list import ListView
 from blog.models import Post,Comment
-from django.views.generic import (TemplateView)
+from django.views.generic import (TemplateView,ListView)
 
 # Create your views here.
 
 # a templateview for accessing the templates
 class Aboutview(TemplateView):
     template_name = 'about.html'
+
+# a class to get list of the posts    
+class PostListView(ListView):
+    model = Post
+
+    # Python version of sql for post that are greater than the current date and order them by desecending order
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte = timezone.now().order_by('-published_date'))
+
+
+        
 
 
 
