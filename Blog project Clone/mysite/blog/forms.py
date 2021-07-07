a3ecb503726a99b200b7f16e203c7b27f5df9a18
mickiy123 <mikikinfe345@gmail.com>
2021-07-07 13:10:15 +0300

    added some comments

--- Blog project Clone/mysite/blog/forms.py
+++ Blog project Clone/mysite/blog/forms.py
@@ -2,17 +2,27 @@
 from django import forms
 from django.db.models import fields
 from blog.models import Post,Comment
+from mysite.blog import models
 
 
 
-# a class for creating forms based on the models
+# a class for creating forms for the post based on the models
 class PostForm(forms.ModelForm):
      
-    # connects the model to the form fiekd
+    # connects the model to the form field
     class meta():
         model = Post
         # connects to the fields neccessary
         fields = ('author','title','text')
-        
+
+# a class for creating forms for the comment based on the models
+class CommentForm(forms.ModelForm):
+
+    # connects the model to the form field
+    class meta():
+        model = Comment
+        # connects to the field necessary
+        fields = ('author','text')
+
 
 
