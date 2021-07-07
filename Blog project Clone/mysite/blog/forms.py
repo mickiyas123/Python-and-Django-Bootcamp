29df4d2d183f381c928127a1a4ae4dc6d1e65abd
mickiy123 <mikikinfe345@gmail.com>
2021-07-07 13:04:58 +0300

    in forms.py created a class connecting the model and taking the form fields to be edited

--- Blog project Clone/mysite/blog/forms.py
+++ Blog project Clone/mysite/blog/forms.py
@@ -1,2 +1,18 @@
+# import the neccessary module and models
 from django import forms
+from django.db.models import fields
 from blog.models import Post,Comment
+
+
+
+# a class for creating forms based on the models
+class PostForm(forms.ModelForm):
+     
+    # connects the model to the form fiekd
+    class meta():
+        model = Post
+        # connects to the fields neccessary
+        fields = ('author','title','text')
+        
+
+
