7ba0334bc508a0ec9d79d247a5e76684d135174d
mickiy123 <mikikinfe345@gmail.com>
2021-07-12 22:09:51 +0300

    Created a route to the home page

--- Blog Project Clone1/mysite/blog/urls.py
+++ Blog Project Clone1/mysite/blog/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # route to the home page
+    path('',views.PostListView.as_view(),name="post"),
     # route to the about view
     path('about/',views.AboutView.as_view(),name="about"),
+    
 ]
