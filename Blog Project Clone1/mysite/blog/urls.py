6a58a9f35c12c03f7117ce55b7be0b15088899b4
mickiy123 <mikikinfe345@gmail.com>
2021-07-13 15:37:50 +0300

    Created a url to access crate posts

--- Blog Project Clone1/mysite/blog/urls.py
+++ Blog Project Clone1/mysite/blog/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     # route to the about view
     path('about/',views.AboutView.as_view(),name="about"),
     # route to detail of the posts
-    url(r'^post/(?P<pk>\d+)$',views.PostListView.as_view(),name="post_detail")
+    url(r'^post/(?P<pk>\d+)$',views.PostListView.as_view(),name="post_detail"),
+    # route to creating a new post
+    path('post/new',views.CreatePostView.as_view,name="post_new")
     
 ]
