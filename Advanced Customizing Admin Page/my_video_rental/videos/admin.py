0d71b668603d814e666bfb19265cae31c49bf257
mickiy123 <mikikinfe345@gmail.com>
2021-08-14 12:25:54 +0300

    Added a class

--- Advanced Customizing Admin Page/my_video_rental/videos/admin.py
+++ Advanced Customizing Admin Page/my_video_rental/videos/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
 
-from . import models
+# class for order in our model
+class MovieAdmin(admin.ModelAdmin):
+
+    fields = ['release_year','title','length']
 
 admin.site.register(models.Customer)
-admin.site.register(models.Movies)
+admin.site.register(models.Movies,MovieAdmin)
