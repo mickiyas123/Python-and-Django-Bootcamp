e96db1baa987412766018a658cd261c6c5163f62
mickiy123 <mikikinfe345@gmail.com>
2021-08-14 11:51:39 +0300

    added str method

--- Advanced Customizing Admin Page/my_video_rental/videos/models.py
+++ Advanced Customizing Admin Page/my_video_rental/videos/models.py
@@ -8,10 +8,18 @@ class Movies(models.Model):
     length = models.PositiveIntegerField()
     release_date = models.PositiveIntegerField()
     director = models.CharField(max_length=400)
+    
+    # string representation of an object 
+    def __str__(self):
+        return self.title
 
 # modle for a customer
 class Customer(models.Model):
     first_name = models.CharField(max_length=256)
     last_name = models.CharField(max_length=256)
     phone = models.PositiveIntegerField()
+    
+    # string representation of an object
+    def __str__(self):
+        return self.first_name  +  " "  + self.last_name
 
