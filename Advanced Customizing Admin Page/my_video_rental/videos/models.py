981779ade1b3b3c981517789265213122e9c9f6e
mickiy123 <mikikinfe345@gmail.com>
2021-08-14 11:04:14 +0300

    Created a model

--- Advanced Customizing Admin Page/my_video_rental/videos/models.py
+++ Advanced Customizing Admin Page/my_video_rental/videos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+# model for a movie
+class Movies(models.Model):
+    title = models.CharField(max_length=256)
+    length = models.PositiveIntegerField()
+    release_date = models.PositiveIntegerField()
+    director = models.CharField(max_length=400)
+
+# modle for a customer
+class Customer(models.Model):
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    phone = models.PositiveIntegerField()
+    
