856527a696556d0010d0608119cdd0f8385ba5c7
mickiy123 <mikikinfe345@gmail.com>
2021-08-14 09:40:40 +0300

    Added Debug module

--- Django2/ProTwo/ProTwo/settings.py
+++ Django2/ProTwo/ProTwo/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # python module for debugging
+    'debug_toolbar',
     'APPTwo'
 ]
 
