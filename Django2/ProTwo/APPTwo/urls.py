96e067fbbd81a696e77fd7a5596a8973317391db
mickiy123 <mikikinfe345@gmail.com>
2021-08-14 09:10:48 +0300

    Url for Debug Tool Bar

--- Django2/ProTwo/APPTwo/urls.py
+++ Django2/ProTwo/APPTwo/urls.py
@@ -1,7 +1,16 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from APPTwo import views
+from django.conf import settings    
 
 urlpatterns=[
     # url(r'^$',views.help,name="help"),
     url(r'^$',views.user,name="user"),
 ]
+
+# checking to see Debug is true
+if settings.DEBUG:
+    import debug_toolbar
+    # creating debug toolbar and conactinating with other urls
+    urlpatterns = [
+        url(r'^__debug__/',include(debug_toolbar.urls))
+    ] + urlpatterns
