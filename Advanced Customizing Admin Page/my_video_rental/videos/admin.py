1b57cf0943994b4babd42d772cac5649664b22d3
mickiy123 <mikikinfe345@gmail.com>
2021-08-14 12:57:21 +0300

    order,search,list

--- Advanced Customizing Admin Page/my_video_rental/videos/admin.py
+++ Advanced Customizing Admin Page/my_video_rental/videos/admin.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-# class for order in our model
+# class for manuplating our models
 class MovieAdmin(admin.ModelAdmin):
-
+   
+    # order  
     fields = ['release_date','title','length','director']
-
+    # search
     search_fields = ['title','length']
+    # list filter
+    list_filter = ['release_date','length']
 
 admin.site.register(models.Customer)
 admin.site.register(models.Movies,MovieAdmin)
